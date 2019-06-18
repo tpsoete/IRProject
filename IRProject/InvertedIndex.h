68e2a4d6f3fb47e6089b03ef77e7056a091e140e
DENO1017 <44641179+DENO1017@users.noreply.github.com>
2019-06-18 22:45:08 +0800

    2245

--- IRProject/InvertedIndex.h
+++ IRProject/InvertedIndex.h
@@ -18,7 +18,7 @@ public:
     
     void AddFile(std::string filename);		// add a text file into the index
     std::vector<size_t> Search(std::string word);	// simple search for a single word
-    std::vector<size_t> TopK(const std::vector<std::string>& words, int k);
+    std::vector<doc_with_score> TopK(const std::vector<std::string>& words, int k);
     std::vector<doc_with_score> get_scores(const std::vector<std::string>& words);
 
 	void Output() const;
